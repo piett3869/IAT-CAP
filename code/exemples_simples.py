241674c7192b0a7b5a9be1ea053c065ca16caece
Tim719 <11026713+Tim719@users.noreply.github.com>
2021-06-15 10:50:03 +0200

    Première solution gloutonne naive

--- code/exemples_simples.py
+++ code/exemples_simples.py
@@ -7,6 +7,7 @@ class Graphe():
         self.a = []
 
         self.sommets_visites = []
+        self.frequences_attribuees = [1]*len(self.d)
 
         self._calculer_liste_arretes()
     
@@ -32,17 +33,36 @@ class Graphe():
                     if val > 0:
                         self.a.append((rown, coln, val))
 
+    def solution_gloutnne_naive(self) -> None:
+        premier_sommet = self.a[0][0]
+        self.sommets_visites.append(premier_sommet)
+
+        for arrete in self.a:
+            print(f"Parcours de l'arrête ({arrete[0]}, {arrete[1]}): {arrete[2]}")
+            sommet_a, sommet_b, valeur = arrete
+
+            if sommet_b in self.sommets_visites:
+                print("SAUTE")
+                if abs(self.frequences_attribuees[sommet_a] - self.frequences_attribuees[sommet_b]) >= valeur:
+                    print("TOUT VA BIEN")
+                    continue
+                else:
+                    print("TOUT NE VA PAS BIEN")
+
+
+            freq_inf = self.frequences_attribuees[sommet_b] = self.frequences_attribuees[sommet_a] - valeur
+            freq_sup = self.frequences_attribuees[sommet_b] = self.frequences_attribuees[sommet_a] + valeur
+
+            self.frequences_attribuees[sommet_b] = 1 if freq_inf > 0 else freq_sup
+
+            self.sommets_visites.append(sommet_b)
+            print(f"Attribution de la fréquence {self.frequences_attribuees[sommet_a] + valeur} au sommet {sommet_b}")
+
+        return self.frequences_attribuees
+
+
 
 if __name__ == '__main__':
-    m1 = np.array([
-        [0, 3, 3], 
-        [3, 0, 3], 
-        [3, 3, 0]
-    ])
-    s1 = np.array([1, 1, 1])
-    g1 = Graphe(m1, s1)
-    # print(g1)
-
     m2 = np.array([
         [0, 3, 0, 3],
         [3, 0, 3, 0],
@@ -52,3 +72,19 @@ if __name__ == '__main__':
     s2 = np.array([1, 1, 1, 1])
     g2 = Graphe(m2, s2)
     print(g2)
+
+    #f = g2.glouton()
+    #print(f)
+
+    m3 = np.array([
+        [0, 2, 0, 0, 4],
+        [0, 0, 1, 0, 4],
+        [0, 0, 0, 3, 0],
+        [0, 0, 0, 0, 5],
+        [0, 0, 0, 0, 0]
+    ])
+    s3 = np.array([1, 1, 1, 1, 1])
+
+    g3 = Graphe(m3, s3)
+    f2 = g3.solution_gloutnne_naive()
+    print(f2)

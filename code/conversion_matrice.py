6f419b5949acd07cc8140be3b7b6254960b4363e
Tim719 <11026713+Tim719@users.noreply.github.com>
2021-06-23 22:32:13 +0200

    Algorithme backtrace non fonctionnel

--- code/conversion_matrice.py
+++ code/conversion_matrice.py
@@ -1,6 +1,11 @@
 import numpy as np
 
 def eclater_matrice(m, d, nsommet):
+    """
+    Découpe le sommet n° `nsommet` dans la matrice `m` en fonction de la demande `d`
+
+    Retourne la matrice `m` et modifie le tableau `d`
+    """
     demande_sommet = d[nsommet]
     contrainte_sommet = m[nsommet][nsommet]
 
@@ -34,6 +39,9 @@ def eclater_matrice(m, d, nsommet):
     return np.vstack(((row_1, row_2, row_3)))
 
 def sommets_a_etendre(m, d):
+    """
+    Retourne le prochain sommer à découper, ou None sinon
+    """
     for k_s, v_s in enumerate(d):
         if v_s > 1 and m[k_s][k_s] > 0:
             return k_s
@@ -41,6 +49,11 @@ def sommets_a_etendre(m, d):
     return None
 
 def convertir_matrice(m, d):
+    """
+    Convertit la matrice m (représentant les arrêtes) en prenant en compte le tableau d de demandes par sommet
+
+    Retourne la matrice `m` et modifie le tableau `d`
+    """
     while True:
         sommet_a_etendre = sommets_a_etendre(m, d)
 
--- code/exemples_simples.py
+++ code/exemples_simples.py
@@ -5,6 +5,9 @@ from functools import wraps
 from time import time
 
 def timing(f):
+    """
+    Wrapper pour timer les appels de fonction
+    """
     @wraps(f)
     def wrap(*args, **kw):
         ts = time()
@@ -16,17 +19,26 @@ def timing(f):
 
 class Graphe():
     def __init__(self, matrice_adjacence, demande_sommets) -> None:
-        copy_demande_sommets = demande_sommets[:]
-        self.m = convertir_matrice(matrice_adjacence, copy_demande_sommets)
-        self.d = copy_demande_sommets
-        self.a = []
+        """Génère un objet Graphe sur lequel exécuter les algorithmes
 
-        self.sommets_visites = []
-        self.frequences_attribuees = [0]*len(self.d)
+        Args:
+            matrice_adjacence (np.array): Matrice d'adjacence du graphe
+            demande_sommets (list): Liste de la demande de chaque sommet
+        """
 
-        self.ordre_parcours = []
+        copy_demande_sommets = demande_sommets[:] # Copie de la liste pour éviter de modifier l'originale
+        self.m = convertir_matrice(matrice_adjacence, copy_demande_sommets) # Matrice d'adjacence convertie
+        self.d = copy_demande_sommets # Demande des sommets
+        self.a = [] # Liste des arrêtes 2 à 2 avec leur contrainte associée
+
+        self.sommets_visites = [] # Liste des sommets visités pour le DFS
+        self.frequences_attribuees = [0]*len(self.d) # Liste des fréquences attribuées. 0 = pas de fréquence
+
+        self.ordre_parcours = [] # Ordre de parcours DFS
         self._dfs()
         self._calculer_liste_arretes()
+
+        self.span_glouton = 0
     
     def __str__(self) -> str:
         rep = 'Graphe:\n'
@@ -39,6 +51,8 @@ class Graphe():
         return rep
 
     def _calculer_liste_arretes(self) -> None:
+        """Construction de la liste des arrêtes 2 à 2 du graphe
+        """
         for rown, arr in enumerate(np.triu(self.m)):
             for coln, val in enumerate(arr):
                 if coln > rown:
@@ -46,14 +60,75 @@ class Graphe():
                         self.a.append((rown, coln, val))
 
     def _dfs(self, node=0):
+        """Construction du parcours DFS du graphe
+
+        Args:
+            node (int, optional): Noeud de départ. Defaults to 0.
+        """
         if node not in self.ordre_parcours:
             self.ordre_parcours.append(node)
             for n, v in enumerate(self.m[node]):
                 if v > 0:
                     self._dfs(node=n)
+    
+    def calculer_frequences_possibles_set(self, node, span_max) -> list:
+        """Pour le noeud en question, calcul de la liste des fréquences possibles en fonction du span max et des voisins
+
+        Args:
+            node (int): Noeud dont on veut connaître la liste des fréquences possibles
+            span_max (int): Maximum du span
+
+        Returns:
+            list: Liste des fréquences possibles
+        """
+        frequences_possibles = set(range(1, span_max+1))
+        frequences_impossibles = set()
+        # On utilise des sets pour pouvoir utiliser les opérations d'union et de différence
+
+        for neighbor, val in enumerate(self.m[node]):
+            freq_neighbor = int(self.frequences_attribuees[neighbor])
+            if val != 0 and freq_neighbor != 0:
+                frequences_impossibles = frequences_impossibles.union(range(int(freq_neighbor - val), int(freq_neighbor + val) + 1))
+
+        return list(frequences_possibles - frequences_impossibles)
+
+    def calculer_frequences_possibles(self, node, span_max) -> list:
+        """Version 1 de la méthode, en n'utilisant pas de sets
+
+        Args:
+            node (int): Noeud dont on veut connaître la liste des fréquences possibles
+            span_max (int): Maximum du span
+
+        Returns:
+            list: Liste des fréquences possibles
+        """
+
+        constraints_up, ensemble_up = [], []
+        constraints_down, ensemble_down = [], []
+        for neighbor, val in enumerate(self.m[node]):
+            if val != 0 and self.frequences_attribuees[neighbor] != 0:
+                constraints_up.append(self.frequences_attribuees[neighbor] + val)
+                constraints_down.append(self.frequences_attribuees[neighbor] - val)
+
+        c_up = int(max(constraints_up))
+        c_down = int(min(constraints_down))
+
+        if c_down > 0:
+            ensemble_down = list(range(1, c_down+1))
+        
+        if c_up < span_max:
+            ensemble_up = list(range(c_up, span_max+1))
+
+        return ensemble_down + ensemble_up
+
 
     @timing
     def solution_gloutonne(self) -> list:
+        """Résout le graphe en utilisant une solution gloutonne
+
+        Returns:
+            list: Liste des fréquences attribuées aux noeuds
+        """
         self.frequences_attribuees[0] = 1
 
         for current_node in self.ordre_parcours[1:]:
@@ -63,22 +138,88 @@ class Graphe():
 
             for n, val in enumerate(self.m[current_node]):
                 if val != 0 and self.frequences_attribuees[n] != 0:
-                    freq = max(freq, self.frequences_attribuees[n] + val)
+                    freq = int(max(freq, self.frequences_attribuees[n] + val))
                     freq_min.append(self.frequences_attribuees[n] - val)
 
             if min(freq_min) > 0:
-                self.frequences_attribuees[current_node] = min(freq_min)
+                self.frequences_attribuees[current_node] = int(min(freq_min))
             else:
                 self.frequences_attribuees[current_node] = freq
 
         
         return self.frequences_attribuees
 
+    def solution_retour_sur_trace(self, span_max):
+        """Résout le problème d'attribution des fréquences en utilisant un retour sur trace
+
+        Args:
+            span_max (int): Span max autorisé pour la coloration
+
+        Returns:
+            list or False: Liste des fréquences attribuées au noeud. Faux si pas de solution trouvée
+        """
+        self.frequences_attribuees = [0]*len(self.d)
+        self.solution_gloutonne()
+        self.span_glouton = self.span()
+
+        print("Le span max est : %d" % self.span_glouton)
+
+        self.frequences_attribuees = [0]*len(self.d)
+
+        retval = self._solution_retour_sur_trace(node_index=self.ordre_parcours[0], span_max=span_max)
+
+        if retval:
+            return self.frequences_attribuees
+        else:
+            print("Pas de solution trouvée")
+            return False
+    
+    def _solution_retour_sur_trace(self, node_index, span_max):
+        """Méthode cachée appelée par la fonction du dessus, qui implémente la vraie logique du retour sur trace
+
+        Args:
+            node_index (int): index du noeud (dans l'ordre de parcours DFS) à colorer
+            span_max (int): span max autorisé pour colorer le graphe
+
+        Returns:
+            bool: Vrai si on a terminé de colorer le graphe de manière valide, Faux sinon
+        """
+
+        if node_index == len(self.d):
+            print("Fin de parcours")
+            return self.test_solution_valide()
+        
+        node = self.ordre_parcours[node_index]
+        
+        print("(%02d/%02d) Traitement du noeud %d" % (node_index, len(self.d),  node))
+
+        frequences_possibles = self.calculer_frequences_possibles_set(node, span_max)
+        print("Fréquences possibles : ", frequences_possibles)
+
+        for frequence in frequences_possibles:
+            self.frequences_attribuees[node] = frequence
+            print("Attribution de la fréquence %d au noeud %d" % (frequence, node))
+            if self._solution_retour_sur_trace(node_index + 1, span_max):
+                return True
+            
+        self.frequences_attribuees[node_index] = 0
+        return False
+
     def span(self):
-        return max(self.frequences_attribuees)
+        """Retourne le span du graphe
 
+        Returns:
+            int: Le span du graphe
+        """
+        print(self.frequences_attribuees)
+        return int(max(self.frequences_attribuees))
 
-    def solution_gloutnne_naive(self) -> list:
+    def solution_gloutonne_naive(self) -> list:
+        """Solution gloutonne pas vraiment gloutonne. Version 1
+
+        Returns:
+            list: Liste des fréquences attribuées
+        """
         premier_sommet = self.a[0][0]
         self.sommets_visites.append(premier_sommet)
 
@@ -102,14 +243,22 @@ class Graphe():
         return self.frequences_attribuees
 
     def test_solution_valide(self):
+        """Teste si la solution trouvée respecte bien toutes les contraintes
+
+        Returns:
+            bool: Vrai ou Faux si le graphe est valide ou pas
+        """ 
         for sommet_a, sommet_b, valeur in self.a:
             if abs(self.frequences_attribuees[sommet_a] - self.frequences_attribuees[sommet_b]) < valeur:
                 print(f"SOLUTION INVALIDE : ({sommet_a}, {sommet_b}) devrait être au minimum {valeur} mais est {abs(self.frequences_attribuees[sommet_a] - self.frequences_attribuees[sommet_b])}")
-                return
+                return False
         
         print("SOLUTION VALIDE")
+        return True
 
 def charger_jeu_donnees(liste_m, liste_dem):
+    """Charge les jeux de données de l'énoncé
+    """
     matrices = []
     demandes = []
 
@@ -126,6 +275,9 @@ def charger_jeu_donnees(liste_m, liste_dem):
     return matrices, demandes
 
 if __name__ == '__main__':
+    """
+    # Charger le jeu de données demandé dans le sujet
+
     matrices, demandes = charger_jeu_donnees(["m1.txt", "m2.txt", "m3.txt"], "dem.txt")
 
     couples = [
@@ -150,10 +302,27 @@ if __name__ == '__main__':
         span = graphe.span()
         graphe.test_solution_valide()
 
-        print("\tSpan: %d" % span)
- 
+        print("\tSpan: %d" % span) """
 
-    """ m4 = np.array([
+    # Matrice simple de l'exemple 3
+    
+    m3 = np.array([
+        [2, 1, 0, 0, 1],
+        [1, 2, 1, 0, 0],
+        [0, 1, 2, 1, 0],
+        [0, 0, 1, 2, 1],
+        [1, 0, 0, 1, 2]
+    ])
+
+    s3 = [2, 2, 2, 2, 2]
+
+    g3 = Graphe(m3, s3)
+
+    f3_b = g3.solution_retour_sur_trace(span_max=9)
+    print(f3_b)
+
+    """
+    m4 = np.array([
         [0, 3, 0, 2, 0, 0, 0, 10],
         [3, 0, 1, 2, 0, 0, 0, 0],
         [0, 1, 3, 4, 0, 0, 0, 0],
@@ -162,12 +331,13 @@ if __name__ == '__main__':
         [0, 0, 0, 4, 3, 0, 1, 0],
         [0, 0, 0, 6, 0, 1, 2, 8],
         [10, 0, 0, 5, 0, 0, 8, 0],
-    ])
+    ], dtype=np.int16)
     s4 = [1, 1, 3, 1, 2, 1, 5, 1]
     g4 = Graphe(m4, s4)
 
-    f4 = g4.solution_gloutonne()
+    f4 = g4.solution_retour_sur_trace()
     span4 = g4.span()
     print(f4)
+    print("Span du graphe: %d" % span4)
     g4.test_solution_valide()
-    print("Span du graphe: %d" % span4) """
+    """
